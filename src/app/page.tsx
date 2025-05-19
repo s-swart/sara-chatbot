ad264e6419e6f15a6da8557d2676d39c70629122
s-swart <73905973+s-swart@users.noreply.github.com>
2025-05-18 20:48:28 -0400

    adding session ID, fix bug on session ID

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,10 +45,12 @@ export default function Home() {
   useEffect(() => {
     let sid = localStorage.getItem('sessionId')
     if (!sid) {
-      // Simple UUID v4 generator (not cryptographically secure)
-      sid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (Number(c) ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (Number(c) / 4)))).toString(16)
-      )
+      // UUID v4 generator using crypto.getRandomValues (TypeScript-safe)
+      sid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        const r = crypto.getRandomValues(new Uint8Array(1))[0] % 16
+        const v = c === 'x' ? r : (r & 0x3) | 0x8
+        return v.toString(16)
+      })
       localStorage.setItem('sessionId', sid)
     }
     setSessionId(sid)
